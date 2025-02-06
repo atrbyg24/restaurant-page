2dce176f38b7c011129cee7ae4c571c60c550ced
atrbyg24 <artwan520@gmail.com>
2025-02-06 15:09:17 -0500

    Add menu images and js module for menu button

--- src/index.js
+++ src/index.js
@@ -44,6 +44,61 @@ const menuBtn = (function() {
     content.replaceChildren();
 
     const menu = document.createElement("div");
+    menu.textContent = "Menu";
+    content.appendChild(menu);
+
+    const itemOne = document.createElement("div");
+    itemOne.textContent = "Grilled Tiger Fish";
+    const itemOneImg = document.createElement("img");
+    itemOneImg.src = "src/Grilled_Tiger_Fish.webp";
+    const itemOneDesc = document.createElement("div");
+    itemOneDesc.textContent = "A fish kebab grilled over an open flame.";
+    const itemOnePrice = document.createElement("div");
+    itemOnePrice.textContent = "$4.00";
+    itemOne.appendChild(itemOneImg);
+    itemOne.appendChild(itemOneDesc);
+    itemOne.appendChild(itemOnePrice);
+    content.appendChild(itemOne);
+
+    const itemTwo = document.createElement("div");
+    itemTwo.textContent = "Honey Char Siu";
+    const itemTwoImg = document.createElement("img");
+    itemTwoImg.src = "src/Honey_Char_Siu.webp";
+    const itemTwoDesc = document.createElement("div");
+    itemTwoDesc.textContent = "A type of roasted meat from Liyue.";
+    const itemTwoPrice = document.createElement("div");
+    itemTwoPrice.textContent = "$10.00";
+    itemTwo.appendChild(itemTwoImg);
+    itemTwo.appendChild(itemTwoDesc);
+    itemTwo.appendChild(itemTwoPrice);
+    content.appendChild(itemTwo);
+
+    const itemThree = document.createElement("div");
+    itemThree.textContent = "Jueyun Chili Chicken";
+    const itemThreeImg = document.createElement("img");
+    itemThreeImg.src = "src/Jueyun_Chili_Chicken.webp";
+    const itemThreeDesc = document.createElement("div");
+    itemThreeDesc.textContent = "Chicken in a dressing, served cold.";
+    const itemThreePrice = document.createElement("div");
+    itemThreePrice.textContent = "$8.00";
+    itemThree.appendChild(itemThreeImg);
+    itemThree.appendChild(itemThreeDesc);
+    itemThree.appendChild(itemThreePrice);
+    content.appendChild(itemThree);
+
+    const itemFour = document.createElement("div");
+    itemFour.textContent = "Rice Buns";
+    const itemFourImg = document.createElement("img");
+    itemFourImg.src = "src/Rice_Buns.webp";
+    const itemFourDesc = document.createElement("div");
+    itemFourDesc.textContent = "Soft and fluffy buns.";
+    const itemFourPrice = document.createElement("div");
+    itemFourPrice.textContent = "5.00";
+    itemFour.appendChild(itemFourImg);
+    itemFour.appendChild(itemFourDesc);
+    itemFour.appendChild(itemFourPrice);
+    content.appendChild(itemFour);
+
 })();
 
 const aboutBtn = (function() {
